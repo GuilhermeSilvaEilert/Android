b87c79b70ce393ae3f01afdf50a62999ce8a567d
guilherme.eilert <gui.eilert@gmail.com>
2022-12-01 09:54:30 -0300

    singleton pronto

--- bhaskaratp/lib/CamadaDeNegocio/calculodoisfatoresImpl.dart
+++ bhaskaratp/lib/CamadaDeNegocio/calculodoisfatoresImpl.dart
@@ -1,10 +1,21 @@
 import 'package:bhaskaratp/CamadaDeNegocio/Interface/CalculoDoisFatores.dart';
 
 class CalculoDoisFatoresImpl{
+  
+  _CalculoDoisFatoresImpl(){}
+
+  static CalculoDoisFatoresImpl? instancia;
+
+  CalculoDoisFatoresImpl? getInstancia(){
+    if(instancia == null) {
+      instancia = CalculoDoisFatoresImpl();
+    }
+    return instancia;
+    }
 
   CalculoDoisFatores? calculodoisfatores;
 
-  CalculoDoisFatoresImpl(CalculoDoisFatores? calculodoisfatores){
+  CalculoDoisFatoresImpl({CalculoDoisFatores? calculodoisfatores}){
     this.calculodoisfatores = calculodoisfatores;
   }
 
